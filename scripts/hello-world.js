334662129e70450c63b93b9c2d3652cb66b76d95
muhammadalvin92 <alvinbomber87@gmail.com>
2022-10-15 14:09:05 +0700

    Komentar

--- scripts/hello-world.js
+++ scripts/hello-world.js
@@ -1,1 +1,5 @@
-document.writeln("Hello World")
+// KOMENTAR DI JS 1 BARIS
+document.writeln("<p>Hello World</p>");
+
+/* KOMEN DI JS LEBIH DARI 1 BARIS */
+document.writeln("<p>Hello World Lagi</p>");
